ea42388c2995ca1b285831a8c2aa8250f92fe07c
Rick Busarow <rickbusarow@gmail.com>
2020-06-14 22:58:57 -0500

    update JUnit5 to 5.6.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -143,7 +143,7 @@ object Libs {
   object JUnit {
     const val jUnit4 = "junit:junit:4.12"
 
-    private const val version = "5.6.0"
+    private const val version = "5.6.2"
 
     const val core = "org.junit.jupiter:junit-jupiter:$version"
     const val api = "org.junit.jupiter:junit-jupiter-api:$version"
